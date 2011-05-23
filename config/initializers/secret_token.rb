a451306252fc99d06f756e07356cdfa6187b09d2
Yinchuan Song <songyinchuan@gmail.com>
2011-05-23 15:40:36 -0400

    New Trip Planner in Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripPlanner::Application.config.secret_token = 'f30e2eda62c5c21bb73bafcb7ff6ef39effc08e8034d6ab70d8eae88af94efe3f05c744bf56c9ce0feb3286839aef44ea2548a64d734f22e80e14e3ead14f474'
